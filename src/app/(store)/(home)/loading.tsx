d4fd1e2e61721ad8667b470285727acbff60fd95
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-12-10 23:13:23 +0000

    Add skeleton loading component, integrate tailwind-merge, and simulate API delay

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,7 +1,11 @@
+import { Skeleton } from '@/app/components/skeleton'
+
 export default function HomeLoading() {
   return (
-    <div>
-      <h1>Loading...</h1>
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 h-[800px]" />
+      <Skeleton className="col-span-3 row-span-3" />
+      <Skeleton className="col-span-3 row-span-3" />
     </div>
   )
 }
