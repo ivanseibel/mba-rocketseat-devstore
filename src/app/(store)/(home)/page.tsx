b30551e6438afe5e2b91f801774cd52ca9c624b1
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-12-10 22:48:11 +0000

    Fetch and display featured products on the home page, update product image paths, and enhance API URL construction

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,16 +1,28 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function HomePage() {
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api('/products/featured')
+
+  const products = await response.json()
+
+  return products
+}
+
+export default async function HomePage() {
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProducts()
+
   return (
     <div className="grid max-h-[800px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
+        href={`/products/${highlightedProduct.slug}`}
         className="relative group col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-500"
-          src="/sweatshirt-ai-side.png"
+          src={highlightedProduct.image}
           width={860}
           height={860}
           quality={100}
@@ -18,54 +30,44 @@ export default function HomePage() {
         />
 
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Sweatshirt AI Side</span>
+          <span className="text-sm truncate">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            $59
+            {highlightedProduct.price.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+              minimumFractionDigits: 0,
+            })}
           </span>
         </div>
       </Link>
 
-      <Link
-        href="/"
-        className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline"
-      >
-        <Image
-          className="group-hover:scale-105 transition-transform duration-500"
-          src="/sweatshirt-ia-p-devs.png"
-          width={860}
-          height={860}
-          quality={100}
-          alt=""
-        />
+      {otherProducts.map((product) => (
+        <Link
+          key={product.id}
+          href={`/products/${product.slug}`}
+          className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline"
+        >
+          <Image
+            className="group-hover:scale-105 transition-transform duration-500"
+            src={product.image}
+            width={860}
+            height={860}
+            quality={100}
+            alt=""
+          />
 
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Sweatshirt AI Side</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            $59
-          </span>
-        </div>
-      </Link>
-
-      <Link
-        href="/"
-        className="relative group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline"
-      >
-        <Image
-          className="group-hover:scale-105 transition-transform duration-500"
-          src="/sweatshirt-java.png"
-          width={860}
-          height={860}
-          quality={100}
-          alt=""
-        />
-
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Sweatshirt AI Side</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            $59
-          </span>
-        </div>
-      </Link>
+          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+            <span className="text-sm truncate">{product.title}</span>
+            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {product.price.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 0,
+              })}
+            </span>
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
--- src/data/api.ts
+++ src/data/api.ts
@@ -2,7 +2,8 @@ import { env } from '../env'
 
 export function api(path: string, init?: RequestInit) {
   const baseURL = env.NEXT_PUBLIC_API_BASE_URL
-  const url = new URL(path, baseURL)
+  const apiPrefix = '/api'
+  const url = new URL(baseURL.concat(apiPrefix, path))
 
   return fetch(url, init)
 }
