256fa7237e0b5b629e78c53fff6cd94c3767b954
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-12-11 14:28:29 +0000

    Implement product details page with dynamic data fetching and API route

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,33 +1,67 @@
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
 import Image from 'next/image'
 
-export default function ProductDetailsPage() {
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+
+  const product = await response.json()
+
+  return product
+}
+
+export default async function ProductDetailsPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+
+  console.log('Slug:', slug)
+
+  const product = await getProduct(slug)
+
+  const parcelsPrice = Number(product.price) / 12
+
   return (
     <div className="relative grid max-h-[800px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
         <Image
-          src="/t-shirt-dowhile-2022.png"
+          src={product.image}
           width={860}
           height={860}
           quality={100}
-          alt=""
+          alt={product.title}
         />
       </div>
 
       <div className="flex flex-col justify-center px-12">
-        <h1 className="text-3xl font-bold leading-tight">
-          DoWhile 2022 T-Shirt
-        </h1>
+        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
 
         <p className="mt-2 leading-relaxed text-zinc-400">
-          Sweatshirt made with 88% cotton and 12% polyester.
+          {product.description}
         </p>
 
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            $120
+            {product.price.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+              minimumFractionDigits: 0,
+            })}
           </span>
           <span className="text-sm text-zinc-400">
-            12x $10 without interest with Mastercard
+            12x{' '}
+            {parcelsPrice.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+              minimumFractionDigits: 0,
+            })}{' '}
+            without interest with Mastercard
           </span>
         </div>
 
@@ -37,25 +71,25 @@ export default function ProductDetailsPage() {
           <div className="flex gap-2">
             <button
               type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 hover:bg-zinc-800/50 text-sm font-semibold"
             >
               S
             </button>
             <button
               type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 hover:bg-zinc-800/50 text-sm font-semibold"
             >
               M
             </button>
             <button
               type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 hover:bg-zinc-800/50 text-sm font-semibold"
             >
               L
             </button>
             <button
               type="button"
-              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 text-sm font-semibold"
+              className="flex h-9 w-14 items-center justify-center rounded-full border border-zinc-700 bg-zinc-800 hover:bg-zinc-800/50 text-sm font-semibold"
             >
               XL
             </button>
@@ -64,7 +98,7 @@ export default function ProductDetailsPage() {
 
         <button
           type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
+          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 hover:bg-emerald-600/50 font-semibold text-white"
         >
           Add to cart
         </button>
---
+++ src/app/api/products/[slug]/route.ts
@@ -0,0 +1,23 @@
+import data from '../data.json'
+import { z } from 'zod'
+
+export async function GET(
+  _: Request,
+  { params }: { params: Promise<{ slug: string }> },
+) {
+  const { slug } = await params
+
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const parsedSlug = z.string().parse(slug)
+
+  const product = data.products.find((product) => product.slug === parsedSlug)
+
+  if (!product) {
+    return Response.json({ message: 'Product not found' }, { status: 400 })
+  }
+
+  console.log('Product:', product)
+
+  return Response.json(product)
+}
