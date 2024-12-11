195bdbc10415120745fc7eee1e4c857b27005d66
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-12-11 18:03:11 +0000

    Enhance search page layout with product display and styling for sweatshirt results

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,7 +1,47 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 export default function SearchPage() {
   return (
-    <div>
-      <h1>Search page</h1>
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Results for: <span className="font-semibold">Sweatshirt</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Link
+          key={'1'}
+          href={`/product/sweatshirt-ai-side`}
+          className="relative group rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-baseline"
+        >
+          <Image
+            className="group-hover:scale-105 transition-transform duration-500"
+            src={'/sweatshirt-never-stop-learning 1.png'}
+            width={480}
+            height={480}
+            quality={70}
+            alt=""
+          />
+
+          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+            <span className="text-sm truncate">
+              Sweatshirt Never Stop Learning
+            </span>
+            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              {/* {product.price.toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 0,
+              })} */}
+              {Number(129).toLocaleString('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 0,
+              })}
+            </span>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
