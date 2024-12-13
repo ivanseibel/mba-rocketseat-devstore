fd372a73f91375bdae16d5006fa5f84f4d31e87d
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-12-13 18:04:50 +0000

    Refactor Cypress tests to use beforeEach for consistent page visits

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,6 +1,9 @@
 describe('add product to cart', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
   it("should be able to navigate to the produc's page and add it to the cart", () => {
-    cy.visit('http://localhost:3000')
     cy.get('a[href^="/product"').first().click()
 
     cy.location('pathname').should('include', '/product')
@@ -10,7 +13,6 @@ describe('add product to cart', () => {
   })
 
   it('should not count a duplicate product in the cart', () => {
-    cy.visit('http://localhost:3000')
     cy.get('a[href^="/product"').first().click()
 
     cy.location('pathname').should('include', '/product')
@@ -23,7 +25,6 @@ describe('add product to cart', () => {
 
   it('should be able to search for a product and add it to the cart', () => {
     const productTitle = 'Sweatshirt Never Stop Learning'
-    cy.visit('http://localhost:3000')
     cy.get('input[name="q"]').type(productTitle)
     cy.get('input[name="q"]').parent('form').submit()
 
